fae117d6db65260a07739db5cf8b14a34c938471
mgirlene <Mgirlene.silva0908@gmail.com>
2020-03-13 14:02:38 -0300

    Sem conectar o banco

---
+++ src/main/java/com/mballem/curso/boot/EstoqueMvcApplication.java
@@ -0,0 +1,13 @@
+package com.mballem.curso.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueMvcApplication.class, args);
+	}
+
+}
